8c2272cf9e6fa50ceb48398a8523194177d3e53d
ssehuun <shn413@naver.com>
2023-04-01 18:46:10 +0900

    리뷰, 구매건수 텍스트 데이터를 숫자로 변경하여 저장

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+import re
 import warnings
 import pprint
 import shutil
@@ -42,7 +43,7 @@ def searchKeyword():
         return
 
 
-def getReviewCnt():
+def getData():
     wb = Workbook()
     for k, v in top500_dic.items():
         encText = urllib.parse.quote(v)
@@ -85,16 +86,15 @@ def getReviewCnt():
                 pass
             try:
                 rev_cnt = driver.find_element(By.XPATH, rev_path).text
-                li.append(rev_cnt)
+                li.append(re.sub("\D", "", rev_cnt))
             except NoSuchElementException:
-                li.append('0')
+                li.append(0)
                 pass
-                # print("No review")
             try:
                 buy_cnt = driver.find_element(By.XPATH, buy_path).text
-                li.append(buy_cnt)
+                li.append(re.sub("\D", "", buy_cnt))
             except NoSuchElementException:
-                li.append('0')
+                li.append(0)
                 pass
             try:
                 itemLink = driver.find_element(By.XPATH, itemLink_path).get_attribute('href')
@@ -103,7 +103,8 @@ def getReviewCnt():
                 pass
             # item_list.append(li)
             ws.append(li)
-    wb.save('C:\\Users\\user\\.virtualenvs\\naverdatalabtop500.xlsx')
+        print(k, v, "완료")
+    wb.save('C:\\Users\\LG\\.virtualenvs\\naverdatalabtop500.xlsx')
     wb.close()
         # pprint.pprint(item_list)
     return
@@ -161,7 +162,7 @@ def writeExcel():
     wb.save('C:\\Users\\user\\.virtualenvs\\naverdatalabtop500.xlsx')
     wb.close()
 
-
+start = time.time()
 # def getRankKeyword():
 options = Options()
 options.add_experimental_option("excludeSwitches", ["enable-logging"]) #크롬 종료되지 않는 상태 유지
@@ -183,14 +184,16 @@ time.sleep(1)
 driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/span').click() #분야 클릭
 time.sleep(0.5)
 
-driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/ul/li[1]/a').click() #패션의류 클릭
+driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/ul/li[9]/a').click() #패션의류 클릭, 마지막 분야
+time.sleep(0.5)#//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/ul/li[9]/a
+#//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/ul/li[9]/a #생활건강
+
+driver.find_element(By.XPATH, f'//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[2]/span').click() # 2분류 클릭
 time.sleep(0.5)
 
-# driver.find_element(By.XPATH, f'//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[2]/span').click() # 2분류 클릭
-# time.sleep(0.5)
-
-# driver.find_element(By.XPATH, f'//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[2]/ul/li[3]/a').click() # 남성의류 클릭
-# time.sleep(0.5)
+driver.find_element(By.XPATH, f'//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[2]/ul/li[8]/a').click() # 여성의류 클릭
+time.sleep(0.5)#//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[2]/ul/li[25]/a
+# //*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[2]/ul/li[8]/a #반려동물
 
 # driver.find_element(By.XPATH, f'//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[3]/span').click() # 3분류 클릭
 # time.sleep(0.5)
@@ -202,7 +205,7 @@ driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div/div[1]/div/a').clic
 time.sleep(1)
 
 for i in range(0, 1) : # 25페이지까지
-    for j in range(1, 21) : # 20개씩
+    for j in range(1, 5) : # 20개씩
         path = f'//*[@id="content"]/div[2]/div/div[2]/div[2]/div/div/div[1]/ul/li[{j}]/a'
         result = driver.find_element(By.XPATH, path).text
         top500_dic[result.split('\n')[0]] = result.split('\n')[1]
@@ -210,16 +213,18 @@ for i in range(0, 1) : # 25페이지까지
         # time.sleep(0.1)
         # ws.append(result.split('\n'))
     
-    # driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/a[2]').click()
+    driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/a[2]').click()
     time.sleep(0.1)
-# print(top500_dic)
+print(top500_dic)
 # driver.close()
 # driver.quit()
 print("네이버 데이터랩 Top 500 키워드 뽑기 완료")
 
 # searchKeyword()
-getReviewCnt()
+getData()
 # writeExcel()
 # if __name__ == '__main__':
     # getRankKeyword()
     
+end = time.time()
+print("총시간: ", end-start)
